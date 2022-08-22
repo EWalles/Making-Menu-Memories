d423600bf892881ef3686d4cec51b0448c8aa6b2
EWalles <ejomcdonald@gmail.com>
2022-08-22 11:03:48 -0400

    working on JS code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+const mealContainer = document.getElementById('btn');
 
+fetchbtn.addEventListener('click', () => {
+    fetch('www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(res => {
+        createMeal(res.meals[0])
+    
+    })
+    });
